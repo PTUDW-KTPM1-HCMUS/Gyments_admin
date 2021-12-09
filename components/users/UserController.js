e8a4cad65c530cb0dd3f012e462594e56cc1d544
hoanghuugiap241 <hoanghuugiap241@gmail.com>
2021-12-09 18:05:10 +0700

    final commit lab04

--- components/users/UserController.js
+++ components/users/UserController.js
@@ -10,7 +10,7 @@ class UserController{
             let previous = Math.ceil(parseInt(currentPage)-1)<1? 1:Math.ceil(parseInt(currentPage)-1);
             let next = Math.ceil(parseInt(currentPage)+1) > pages.length?pages.length: Math.ceil(parseInt(currentPage)+1);
             let length = true; // check if search for 1 user or multiple
-            if (username.length != 0)
+            if (username.length != 0 || accounts.length === 1)
                 length = false;
             res.render('users/views/management',{accounts, activeAccounts, bannedAccounts, pages, currentPage,previous,next, option, username, length});
         }catch (err){
